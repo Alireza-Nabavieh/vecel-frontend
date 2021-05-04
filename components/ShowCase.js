fa43056b1e21daf9a7976cafeacd3e3fc0e98c72
a.nabavieh <nabavieh@gmail.com>
2021-05-04 18:29:05 +0430

    name changed

--- components/ShowCase.js
+++ components/ShowCase.js
@@ -1,6 +1,6 @@
-import styles from '@/styles/ShowCase.module.css'
+import styles from '@/styles/Showcase.module.css'
 
-export default function ShowCase() {
+export default function Showcase() {
     return (
         <div className={styles.showcase}>
             <h1>خوش آمدید!</h1>
